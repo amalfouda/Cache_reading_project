2a34589a07ffc4e7e64374c86697a4d409387565
omaranwar1 <98822306+omaranwar1@users.noreply.github.com>
2023-12-07 23:54:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-
-
 #include <iostream>
+#include <string>
 #include <map>
 #include <vector>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cmath>
 using namespace std;
 map<int,int>memory;
+vector<int> seq_of_access;
+
+bool isPowerOfTwo(int number) {
+    return (number > 0 && (log2(number) == round(log2(number))) );
+}
+
 string decimal_binary(int n)
 {
     string binary = "";
@@ -21,13 +27,15 @@ string decimal_binary(int n)
 
     return binary;
 }
-struct cache_Input
+struct cache_entry
 {
     bool valid;
     int index;
-    int Tag;
+    string Tag;
     vector<int>data;
-    cache_Input(int i, int d, int t, bool v) : valid(v), index(i), Tag(t), data(d, 0) {
+    
+    
+    cache_entry(int i, int d, string t, bool v) : index(i), data(d, 0), Tag(t), valid(v) {
        
     }
     
@@ -35,35 +43,79 @@ struct cache_Input
 struct cache
 {
     
-    cache_Input cc;
-    vector<cache_Input>inputs;
+    vector<cache_entry> cache_entries;
     int cycles;
     int cache_size;//total bytes in cache user
-    //
-    int lines;//cache_size/line_size->block calculated 
-    int line_size;//user 
-     //bits number
-        int offset;
-        int idx;
-        int T;
-    cache(int size, int line_s, int c) :  cache_size(size),line_size(line_s), cycles(c) {
-        lines=cache_size/line_size;
+    int lines;//cache_size/line_size->block calculated
+    int line_size;
+    int offset;
+    int idx;
+    int T;
+    
+    int accesses;
+    int hits;
+    int misses;
+    
+    cache(int size, int line_s, int c) :  cache_size(size), line_size(line_s), cycles(c) {
+        lines= cache_size/line_size;
         for (int i = 0; i < lines; i++) {
-            cache_Input entry(i, line_size, 0, 0); 
-            inputs.push_back(entry);
+            cache_entry entry(i, line_size, "", false);
+            cache_entries.push_back(entry);
         }
         offset= int(log2(line_size));
         idx=int(log2(lines));
-        int T=24-(offset+idx);
+        T = 24-(offset+idx);
+    accesses=0;
+    hits=0;
+    misses=0;
     }
     
-    void Cache_Intialization(int Mem_address)
+    void access_sequence()
     {
         
        
-        
-        
+      for (auto mem_address : seq_of_access) {
+      string bin_address=decimal_binary(mem_address);
+          string x=bin_address.substr(0,T);
+         x=bin_address.substr(T,idx);
+        x=bin_address.substr(T+idx,offset);
+      string tag = bin_address.substr(0,T);
+      int index = stoi(bin_address.substr(T,idx),0,2);
+      int disp = stoi(bin_address.substr(T+idx,offset),0,2);
+          bool hit= false;
+       accesses++;
+            if (cache_entries[index].Tag == tag && cache_entries[index].valid)
+                {
+                    hit=true;
+                    hits++;
+                }
+            else
+            {
+                misses++;
+                cache_entries[index].Tag = tag;
+                cache_entries[index].valid = true;
+            }
+            
+            cout << "Access: " << dec<< accesses << endl;
+          cout<< "Getting address: 0x"<<hex <<mem_address<<endl;
+          if (hit) {
+              cout<<"HIT!"<< endl;
+          }
+          else cout<<"MISS!"<< endl;
+            for (auto entry : cache_entries)
+            {
+                cout << "Cache Entry " << dec<<entry.index+1 << ": Valid bit- " << entry.valid << ", Tag bits- " << entry.Tag << endl;
+            }
+            double miss_ratio= (double) misses / accesses;
+            double hit_ratio= (double) hits / accesses;
+            cout << "Total Number of Accesses: " << accesses << endl;
+            cout << "Hit Ratio: " << hit_ratio << endl;
+            cout << "Miss Ratio: " << miss_ratio << endl;
+          double AMAT= ((double)cycles + (miss_ratio * 120));
+            cout << "Average Memory Access Time (AMAT): " << AMAT << " cycles" << endl<<endl;
+        }
     }
+        
 };
 void loading_data(string& filename) {
     ifstream file(filename);
@@ -75,7 +127,8 @@ void loading_data(string& filename) {
             string part;
             while (std::getline(iss, part, ',')) {
             int mem_add=stoi(part);
-             memory[mem_add]=0;
+            memory[mem_add]=0;
+            seq_of_access.push_back(mem_add);
             }
         }
         file.close();
@@ -86,7 +139,27 @@ void loading_data(string& filename) {
 
 
 int main()
-{ 
+{
 
+    int cacheSize, lineSize, cycles;
+    do {
+        cout << "Enter cache size (in bytes) must be a power of 2 in the form 2^n: ";
+        cin >> cacheSize;
+    } while (!isPowerOfTwo(cacheSize));
+    do {
+       cout << "Enter cache line size (in bytes) must be a power of 2 in the form 2^n: ";
+       cin >> lineSize;
+    } while (!isPowerOfTwo(lineSize) || lineSize>=cacheSize);
+       cout << "Enter number of cycles needed to access the cache: ";
+       cin >> cycles;
+    string filename;
+    cout << "Enter the name of the file contatining the access sequence. (eg. input.txt)" << endl;
+    cin >> filename;
+    loading_data(filename);
+    //seq_of_access = {4444,333,111,333,48294,42521,111,28942,123425,1024,532};
+
+ cache my_cache(cacheSize, lineSize, cycles);
+ my_cache.access_sequence();
+ 
     return 0;
 }
