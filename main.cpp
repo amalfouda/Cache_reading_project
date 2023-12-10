fb528a67b34ff6522cf066694d561aa9c96be01a
omaranwar1 <98822306+omaranwar1@users.noreply.github.com>
2023-12-11 01:39:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@
 #include <string>
 #include <cmath>
 using namespace std;
-map<int,int>memory;
-vector<int> seq_of_access;
+
+map<int, int> memory;
+vector<pair<int, char>> seq_of_access; // pair of address and operation (r/w)
+bool writeThrough= false;
+bool writeAllocate= false;
 
 bool isPowerOfTwo(int number) {
-    return (number > 0 && (log2(number) == round(log2(number))) );
+    return (number > 0 && (log2(number) == round(log2(number))));
 }
 
-string decimal_binary(int n)
-{
+string decimal_binary(int n) {
     string binary = "";
 
     if (n < 0 || n >= (1 << 24)) {
@@ -27,97 +29,149 @@ string decimal_binary(int n)
 
     return binary;
 }
-struct cache_entry
-{
+
+struct cache_entry {
     bool valid;
+    bool dirty;
     int index;
     string Tag;
-    vector<int>data;
-    
-    
-    cache_entry(int i, int d, string t, bool v) : index(i), data(d, 0), Tag(t), valid(v) {
-       
-    }
-    
+    vector<int> data;
+
+    cache_entry(int i, int d, string t, bool v, bool dir) : index(i), data(d, 0), Tag(t), valid(v), dirty(dir) {}
 };
-struct cache
-{
-    
+
+struct cache {
     vector<cache_entry> cache_entries;
     int cycles;
-    int cache_size;//total bytes in cache user
-    int lines;//cache_size/line_size->block calculated
+    int cache_size;
+    int lines;
     int line_size;
     int offset;
     int idx;
     int T;
-    
+
     int accesses;
     int hits;
     int misses;
-    
-    cache(int size, int line_s, int c) :  cache_size(size), line_size(line_s), cycles(c) {
-        lines= cache_size/line_size;
+
+    cache(int size, int line_s, int c) : cache_size(size), line_size(line_s), cycles(c) {
+        lines = cache_size / line_size;
         for (int i = 0; i < lines; i++) {
-            cache_entry entry(i, line_size, "", false);
+            cache_entry entry(i, line_size, "", false, false);
             cache_entries.push_back(entry);
         }
-        offset= int(log2(line_size));
-        idx=int(log2(lines));
-        T = 24-(offset+idx);
-    accesses=0;
-    hits=0;
-    misses=0;
+        offset = int(log2(line_size));
+        idx = int(log2(lines));
+        T = 24 - (offset + idx);
+        accesses = 0;
+        hits = 0;
+        misses = 0;
     }
-    
-    void access_sequence()
-    {
+
+    void access_sequence() {
+        int totalcycles=0;
         
-       
-      for (auto mem_address : seq_of_access) {
-      string bin_address=decimal_binary(mem_address);
-          string x=bin_address.substr(0,T);
-         x=bin_address.substr(T,idx);
-        x=bin_address.substr(T+idx,offset);
-      string tag = bin_address.substr(0,T);
-      int index = stoi(bin_address.substr(T,idx),0,2);
-      int disp = stoi(bin_address.substr(T+idx,offset),0,2);
-          bool hit= false;
-       accesses++;
-            if (cache_entries[index].Tag == tag && cache_entries[index].valid)
-                {
-                    hit=true;
-                    hits++;
-                }
-            else
-            {
-                misses++;
-                cache_entries[index].Tag = tag;
-                cache_entries[index].valid = true;
-            }
+        for (auto access : seq_of_access) {
+            int mem_address = access.first;
+            char operation = access.second;
+
+            string bin_address = decimal_binary(mem_address);
+            string tag = bin_address.substr(0, T);
+            int index = stoi(bin_address.substr(T, idx), 0, 2);
+            int disp = stoi(bin_address.substr(T + idx, offset), 0, 2);
+            bool hit = false;
+            accesses++;
+            cout << "Access: " << dec << accesses << endl;
+            cout << "Getting address: 0x" << hex << mem_address << " (" << (operation == 'w' ? "Write" : "Read") << ")" << endl;
             
-            cout << "Access: " << dec<< accesses << endl;
-          cout<< "Getting address: 0x"<<hex <<mem_address<<endl;
-          if (hit) {
-              cout<<"HIT!"<< endl;
-          }
-          else cout<<"MISS!"<< endl;
-            for (auto entry : cache_entries)
-            {
-                cout << "Cache Entry " << dec<<entry.index+1 << ": Valid bit- " << entry.valid << ", Tag bits- " << entry.Tag << endl;
+            if (cache_entries[index].Tag == tag && cache_entries[index].valid) {
+                hit = true;
+         
+                    cout << "HIT!" << endl;
+                hits++;
+                if(operation=='w')
+                {
+                    
+                    if(writeThrough)
+                    {   
+                        totalcycles += cycles+120; //simulating writing to memory and cache at the same time as there is a hit and write policy is write through
+                        cout << "Writing to memory and cache as of Write Through policy....." << endl;
+                    }
+                    else
+                    {
+                        totalcycles +=cycles;
+                        cache_entries[index].dirty=true; //simulating writing to cache only and changing dirty bit to true as write back policy for hit is used
+                        cout << "Writing to cache as of Write Back Policy....." << endl;
+                    }
+                 
+                }
+                else
+                { 
+                    totalcycles +=cycles; //simulating reading from cache as there is a hit and it is a read operation
+                }
+            } else {
+            
+                misses++;
+                    cout << "MISS!" << endl;
+                if(cache_entries[index].dirty == true)
+                {
+                    totalcycles += 120;
+                    cache_entries[index].dirty=false; // simuliating writing cache with dirty bit to memory after miss as data has not been written to memory yet
+                    cout << "Wait... Dirty Cache entry found Writing data in cache entry to memory...." << endl;
+                }
+
+                if(operation!='w')
+                {
+                    cache_entries[index].Tag = tag;
+                    cache_entries[index].valid = true;
+
+                    totalcycles += cycles+120; //reading from memory
+                    
+                }
+                else{
+                    if(writeAllocate)
+                    {   // simulating write allocate miss policy putting data of the address in the cache and writing based on the writing policy
+                        cache_entries[index].Tag = tag;
+                        cache_entries[index].valid = true;
+                        if(writeThrough)
+                        {
+                            totalcycles += cycles+240+cycles;
+                            cout << "Data fetched. Writing to memory and cache as of Write Through policy....." << endl;
+                        }
+                        else
+                        {
+                            totalcycles += cycles+120+cycles;
+                            cache_entries[index].dirty=true;
+                            cout << "Data fetched. Writing to cache as of Write Back Policy....." << endl;
+                        }
+                    }
+                    else
+                    {
+                        totalcycles += cycles+120; // simulating no write allocate for miss writing to the memory directly
+                        cout << "No Data fetched. Writing to memory directly as of no write allocate Policy....." << endl;
+                    }
+                }
             }
-            double miss_ratio= (double) misses / accesses;
-            double hit_ratio= (double) hits / accesses;
+
+            
+            for (auto entry : cache_entries) {
+                cout << "Cache Entry " << dec << entry.index + 1 << ": Valid bit- " << entry.valid << ", Tag bits- " << entry.Tag << endl;
+            }
+            double miss_ratio = (double)misses / accesses;
+            double hit_ratio = (double)hits / accesses;
+        
             cout << "Total Number of Accesses: " << accesses << endl;
+            cout << "Total number cycles spent accessing and writing data for whole sequence:" << totalcycles<< " cycles" <<endl;
             cout << "Hit Ratio: " << hit_ratio << endl;
             cout << "Miss Ratio: " << miss_ratio << endl;
-          double AMAT= ((double)cycles + (miss_ratio * 120));
-            cout << "Average Memory Access Time (AMAT): " << AMAT << " cycles" << endl<<endl;
+            double AMAT = ((double)totalcycles/ accesses);
+            cout << "Average Memory Access Time (AMAT): " << AMAT << " cycles" << endl
+                 << endl;
         }
     }
-        
 };
-void loading_data(string& filename) {
+
+void loading_data(string &filename) {
     ifstream file(filename);
     string line;
 
@@ -126,9 +180,12 @@ void loading_data(string& filename) {
             istringstream iss(line);
             string part;
             while (std::getline(iss, part, ',')) {
-            int mem_add=stoi(part);
-            memory[mem_add]=0;
-            seq_of_access.push_back(mem_add);
+                int mem_add;
+                char operation;
+                stringstream ss(part);
+                ss >> mem_add >> operation;
+                memory[mem_add] = 0;
+                seq_of_access.push_back(make_pair(mem_add, operation));
             }
         }
         file.close();
@@ -137,29 +194,44 @@ void loading_data(string& filename) {
     }
 }
 
-
-int main()
-{
-
+int main() {
     int cacheSize, lineSize, cycles;
+    string writePolicy_hit, writePolicy_miss;
     do {
         cout << "Enter cache size (in bytes) must be a power of 2 in the form 2^n: ";
         cin >> cacheSize;
     } while (!isPowerOfTwo(cacheSize));
     do {
-       cout << "Enter cache line size (in bytes) must be a power of 2 in the form 2^n: ";
-       cin >> lineSize;
-    } while (!isPowerOfTwo(lineSize) || lineSize>=cacheSize);
-       cout << "Enter number of cycles needed to access the cache: ";
-       cin >> cycles;
+        cout << "Enter cache line size (in bytes) must be a power of 2 in the form 2^n: ";
+        cin >> lineSize;
+    } while (!isPowerOfTwo(lineSize) || lineSize >= cacheSize);
+    cout << "Enter number of cycles needed to access the cache: ";
+    cin >> cycles;
+    do {
+           cout << "Enter write policy for hit (w for write-back, t for write-through): ";
+           cin >> writePolicy_hit;
+       } while (writePolicy_hit != "w" && writePolicy_hit != "t");
+    do {
+           cout << "Enter write policy for miss (1 for write-allocate, 2 for no write-allocate): ";
+           cin >> writePolicy_miss;
+       } while (writePolicy_miss != "1" && writePolicy_miss != "2");
+    
+    if(writePolicy_hit=="t")
+        writeThrough=true;
+    
+    if(writePolicy_miss=="1")
+        writeAllocate=true;
+
+    
+    
     string filename;
-    cout << "Enter the name of the file contatining the access sequence. (eg. input.txt)" << endl;
+    cout << "Enter the name of the file containing the access sequence. (eg. input.txt)" << endl;
     cin >> filename;
     loading_data(filename);
-    //seq_of_access = {4444,333,111,333,48294,42521,111,28942,123425,1024,532};
 
- cache my_cache(cacheSize, lineSize, cycles);
- my_cache.access_sequence();
- 
+    cache my_cache(cacheSize, lineSize, cycles);
+    my_cache.access_sequence();
+
     return 0;
 }
+
